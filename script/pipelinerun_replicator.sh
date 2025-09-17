c848834954e1a87103ba6262eddf3b07f9f5e356
Rishab Prasad <riprasad@redhat.com>
2025-09-17 13:29:19 +0100

    fix conflict (#603)

--- script/pipelinerun_replicator.sh
+++ script/pipelinerun_replicator.sh
@@ -159,7 +159,8 @@ for folder in $(echo "$folders" | jq -r '.[]'); do
 
           # Modelmesh has an additional build argument that needs to be updated as well.
           # https://github.com/red-hat-data-services/modelmesh/blob/36ff14bc/.tekton/odh-modelmesh-v2-22-push.yaml#L41-L43
-          if [[ $file == "odh-modelmesh-v${tkn_source_hyphenated_version}-push.yaml" ]]; then
+          if [[ $filename == "odh-modelmesh-v${tkn_source_hyphenated_version}-push.yaml" ]]; then
+            echo "  🔔  updating VERSION in build-args!"
             ${sed_command} -i '/name: build-args/{n;:a;/VERSION=/ {s/VERSION=["]*[^""]*[""]*/VERSION='"$RHOAI_VERSION"'/;b};n;ba}' $file
           fi
 
